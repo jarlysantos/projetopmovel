e7e85573a8ee0b0db2ab16aa5cd11868c51f22cf
caceso222 <claudioandre702@gmail.com>
2025-07-14 15:11:22 -0300

    quaseterminado

---
+++ lib/database.dart
@@ -0,0 +1,34 @@
+import 'package:projarly2/propriedades.dart';
+
+class database{
+  static List propriedades = [
+    Propriedade(
+        preco: '300',
+        modelo: 'Gatinho',
+        cor: 'Rosa Claro',
+        imageUrl:
+        'https://i.pinimg.com/736x/33/3a/15/333a15fecadc047609e0e33c9a7d8edc.jpg',
+    ),
+     Propriedade(
+       preco: 'RS 350',
+       modelo: 'Arco-íris',
+       cor: 'Rosa Claro',
+       imageUrl:
+       'https://i.pinimg.com/736x/1a/f2/d6/1af2d6c2825354f6770ba249ba2bcdcb.jpg',
+     ),
+    Propriedade(
+      preco: 'RS 295',
+      modelo: 'Florzinha',
+      cor: 'Marrom Claro',
+      imageUrl:
+      'https://i.pinimg.com/736x/78/7e/7a/787e7a2b8dd5219ab507dc141145b5b7.jpg',
+    ),
+    Propriedade(
+      preco: 'RS 400',
+      modelo: 'Laço',
+      cor: 'Rosa Claro',
+      imageUrl:
+      'https://i.pinimg.com/736x/8d/91/ab/8d91ab939d607e226464272225d71a82.jpg',
+    ),
+  ];
+}
---
+++ lib/propriedades.dart
@@ -0,0 +1,17 @@
+class Propriedade{
+
+    String imageUrl;
+    String modelo;
+    String cor;
+    String preco;
+
+
+    Propriedade({
+
+     required this.imageUrl,
+     required this.modelo,
+     required this.cor,
+     required this.preco,
+
+});
+}
