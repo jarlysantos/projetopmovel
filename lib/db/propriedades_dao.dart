9b3b1128cc72c7f84ac928f32f7862bc8fba67a9
caceso222 <claudioandre702@gmail.com>
2025-10-20 14:33:32 -0300

    quase laaaa

--- lib/db/propriedades_dao.dart
+++ lib/db/propriedades_dao.dart
@@ -1,22 +1,32 @@
-
+import 'package:dio/dio.dart';
 import 'package:sqflite/sqflite.dart';
 import '../domain/propriedades.dart';
-import 'db_helper.dart';
+//import 'db_helper.dart';
 
+class PropriedadesApi {
+  final dio = Dio();
+  final String baseUrl = 'https://my-json-server.typicode.com./sasaam/fakeinfantil';
 
-class Propiedades_dao{
   Future<List<Propriedade>> ListarPropriedades() async {
-    List<Propriedade> listaPropriedades = [];
-    Database db = await DBHelper().initDB();
+  List<Propriedade> listaPropriedades = [];
+  //Database db = await DBHelper().initDB();
+  //String sql = 'SELECT * FROM PROPRIEDADE;';
+  //var listResult = await db.rawQuery(sql);
 
-    String sql = 'SELECT * FROM PROPRIEDADE;';
-    var listResult = await db.rawQuery(sql);
+  final response = await dio.get('$baseUrl/propertes');
 
-    for (var json in listResult) {
-      Propriedade propriedade = Propriedade.fromJson(json);
-      listaPropriedades.add(propriedade);
-    }
-    return listaPropriedades;
+  if(response.statusCode == 200){
+    var listResult = response.data;
+    print(listResult);
+  for (var json in listResult) {
+  Propriedade propriedade = Propriedade.fromJson(json);
+  listaPropriedades.add(propriedade);
+   }
+  }
+
+  await Future.delayed(Duration(seconds: 3));
+
+  return listaPropriedades;
 
   }
 }
