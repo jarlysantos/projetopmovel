5cd6acb6cefaaf2c2407f73d869a6dded520b65c
sasaaam <sss47@aluno.ifal.edu.br>
2025-11-10 07:03:58 -0300

    Update api-falsa.dart

--- lib/apis/api-falsa.dart
+++ lib/apis/api-falsa.dart
@@ -1,32 +1,31 @@
 import 'package:dio/dio.dart';
-import 'package:sqflite/sqflite.dart';
 import '../domain/propriedades.dart';
-//import 'db_helper.dart';
 
 class PropriedadesApi {
-  final dio = Dio();
-  final String baseUrl = 'https://my-json-server.typicode.com./sasaam/fakeinfantil';
+  final Dio dio = Dio();
+  final String baseUrl = 'https://my-json-server.typicode.com/sasaam/fakeinfantil';
 
-  Future<List<Propriedade>> ListarPropriedades() async {
+  Future<List<Propriedade>> listarPropriedades() async {
     List<Propriedade> listaPropriedades = [];
-    //Database db = await DBHelper().initDB();
-    //String sql = 'SELECT * FROM PROPRIEDADE;';
-    //var listResult = await db.rawQuery(sql);
 
-    final response = await dio.get('$baseUrl/propertes');
+    try {
+      final response = await dio.get('$baseUrl/propriedades');
 
-    if(response.statusCode == 200){
-      var listResult = response.data;
-      print(listResult);
-      for (var json in listResult) {
-        Propriedade propriedade = Propriedade.fromJson(json);
-        listaPropriedades.add(propriedade);
+      if (response.statusCode == 200) {
+        var listResult = response.data;
+        print(listResult);
+
+        for (var json in listResult) {
+          Propriedade propriedade = Propriedade.fromJson(json);
+          listaPropriedades.add(propriedade);
+        }
       }
+    } catch (e) {
+      print('Erro ao buscar propriedades: $e');
     }
 
     await Future.delayed(Duration(seconds: 3));
 
     return listaPropriedades;
-
   }
 }
