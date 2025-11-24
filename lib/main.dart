912aebb179521e707f4a1d674f499cead796e0b7
LauanyB08 <lauanydelmiro08@gmail.com>
2025-11-24 15:35:37 -0300

    fim

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,10 @@ import 'package:projarly2/pages/listar.dart';
 import 'package:projarly2/pages/listar_oculos_page.dart';
 import 'package:projarly2/pages/novaTela.dart';
 
-
 void main() {
   runApp(
     MaterialApp(
       home: ListaOculos(),
     ),
   );
-
-
 }
-
-
-
